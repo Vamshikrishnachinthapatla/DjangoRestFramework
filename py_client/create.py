cc764067b46ed4aa6b439e7d3ef994e33d03313c
vamshikrishna <krishnachvamshi@gmail.com>
2026-04-11 15:43:50 +0530

    permissions-commit

--- py_client/create.py
+++ py_client/create.py
@@ -2,14 +2,12 @@ import requests
 endpoint = "http://localhost:8000/api/products/"
 
 
+
 data = {
     "title":"THIS FIELD IS DONE",
-    "price":32.99
+    "price":32.99,
 }
 
 get_response = requests.post(endpoint, json=data)
-print(get_response.status_code) # See if it's 404, 500, etc.
-if get_response.status_code != 200:
-    print(get_response.text) # This will show the HTML error from Django
-else:
-    print(get_response.json())
+
+print(get_response.json())
